5b7adbb498946c579a523544ce90dc8b21afe63f
Gleb Helgeson <glebh@live.no>
2022-06-12 17:35:09 +0200

    Add presentation of response in ResponsePresentation

--- src/Components/ResponsePresentation/ResponsePresentation.tsx
+++ src/Components/ResponsePresentation/ResponsePresentation.tsx
@@ -1,19 +1,60 @@
 import React from 'react';
 import "./ResponsePresentation.css";
-import { IResponseData } from '../Interfaces/Interfaces';
+import { IConversionRates, IResponseData } from '../Interfaces/Interfaces';
 
 
-
+const simpleObjectToArray = (conversion_rates: IConversionRates) => {
+  const propertyNames = Object.keys(conversion_rates);
+  const propertyValues = Object.values(conversion_rates);
+}
 
 interface IResponsePresentationProps {
   responseData: IResponseData
 }
 
+const getConversionRatesJsx = (conversion_rates: IConversionRates) => {
+  const entries = Object.entries(conversion_rates);
+  const entriesJsx = entries.map(entry => {
+    return (<tr key={entry[0]}>
+      <td>{entry[0]}</td>
+      <td>{entry[1]}</td>
+    </tr>);
+  });
+
+  console.log('entriesJsx: ', entriesJsx);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Currency</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {entriesJsx}
+      </tbody>
+    </table>
+  );
+
+}
+
 const ResponsePresentation = (props: IResponsePresentationProps) => {
+
+  // console.log(props);
+  
+  // let conversionRatesJsx: JSX.Element = {} as JSX.Element;
+  // if(props.responseData.conversion_rates){
+  //   getConversionRatesJsx(props.responseData.conversion_rates);
+  // }
+
   return (
     <div className="response-presentation">
       <h4>ResponseData</h4>
-      {props.responseData.base_code}
+      <h5>{props.responseData.base_code}</h5>
+      <div className="response-table-wrapper">
+        {props.responseData.conversion_rates ? getConversionRatesJsx(props.responseData.conversion_rates) : <p>No Data</p>}
+      </div>
     </div>
   );
 }
